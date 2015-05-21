0ea57bf6e39fba62ccfb5ab6970e76a19c246841
John Siwicki <johnsiwicki@gmail.com>
2015-05-20 22:10:03 -0400

    added custom actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :change]
 
  
   # GET /tasks
@@ -25,6 +25,13 @@ class TasksController < ApplicationController
   # GET /tasks/1/edit
   def edit
   end
+
+def change
+  @task.update_attributes(state: params[:state])
+  respond_to do |format|
+    format.html {redirect_to tasks_path, notice: "Task Update"}
+  end
+end
 
   # POST /tasks
   # POST /tasks.json
